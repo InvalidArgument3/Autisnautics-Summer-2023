72047f9f0ab0a4a7f0a0961bedbe1b449994f979
InvalidArgument3 <InvalidArgument3@gmail.com>
2023-06-30 03:17:05 -0500

    lots of configging

--- .minecraft/kubejs/client_scripts/script.js
+++ .minecraft/kubejs/client_scripts/script.js
@@ -5,8 +5,4 @@ console.info('Hello, World! (You will see this line every time client resources 
 onEvent('jei.hide.items', event => {
 	// Hide items in JEI here
 	// event.hide('minecraft:cobblestone')
-	event.hide(Item.of('ae2:facade').ignoreNBT()),
-	event.hide(Item.of('chiselsandbits:block_bit').ignoreNBT())
-	
-	
 })
